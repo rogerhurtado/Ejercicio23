0bc54c7c4eaff87c7e89923389af8a21fcff211f
roger <roger@DESKTOP-LFIBPUK>
2024-11-09 16:43:41 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Ejercicio23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BotonIntro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BotonIntro
+            // 
+            this.BotonIntro.Location = new System.Drawing.Point(287, 141);
+            this.BotonIntro.Name = "BotonIntro";
+            this.BotonIntro.Size = new System.Drawing.Size(217, 107);
+            this.BotonIntro.TabIndex = 0;
+            this.BotonIntro.Text = "App Introducir Peso";
+            this.BotonIntro.UseVisualStyleBackColor = true;
+            this.BotonIntro.Click += new System.EventHandler(this.BotonIntro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BotonIntro);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BotonIntro;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.Devices;
+
+namespace Ejercicio23
+{
+    public partial class Form1 : Form
+    {
+        /*23. Realizar un programa en el que vayamos introduciendo el peso de los distintos alumnos de
+        una clase.Los pesos se irán introduciendo con InputBox hasta que se introduzca un peso
+        negativo, de manera similar a como se hace en el ejercicio 15.
+        Queremos obtener cuántos alumnos hay en cada uno de los rangos de peso siguientes: (<=50),
+        (>50 y <=65), (>65 y <=80), (>80) así como el tanto por ciento que representan frente al total
+        de alumnos y el peso medio de toda la clase.
+        */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonIntro_Click(object sender, EventArgs e)
+        {
+            int peso, pesoMin, pesoMax;
+
+            peso = int.Parse(Interaction.InputBox("Introduzca un Peso:", "Ejercicio23","0"));
+
+            if(peso > 0)
+            {
+                pesoMax = peso;
+
+
+                pesoMin = peso;
+
+                while (peso > 0)
+                {
+                    if (peso > pesoMax)
+                    {
+                        pesoMax = peso;
+                    }
+                    if (peso < pesoMin)
+                    {
+                        pesoMin = peso;
+
+                    }
+
+
+                peso = int.Parse(Interaction.InputBox("Introduca un Peso:", "Ejercicio23", "0"));//try cach dentro de interaction
+                }
+
+                MessageBox.Show("El Peso máximo es:" + pesoMax + ", y el menor es: " + pesoMin);
+            }
+            else
+            {
+
+                MessageBox.Show("Ha introducido un Peso negativo. No hay máximo ni mínimo");
+            }
+
+        }
+
+    }
+}
